46575d9abd9ac23e707f81e1588acbb2c3fff7d9
Alexander Cleoni <30603679+ACleoni@users.noreply.github.com>
2025-06-20 00:27:14 -0400

    Update components/ui/dashboard/Terminal.tsx
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- components/ui/dashboard/Terminal.tsx
+++ components/ui/dashboard/Terminal.tsx
@@ -14,7 +14,7 @@ export function Terminal({
   return (
     <div className="relative px-6 pt-8 sm:pt-16 md:pr-0 md:pl-16">
       <div className="mx-auto max-w-2xl md:mx-0 md:max-w-none">
-        <div className="w-screen overflow-hidden rounded-tl-xl bg-gray-900">
+        <div className="max-w-full overflow-hidden rounded-tl-xl bg-gray-900">
           <div className="flex bg-gray-800/40 ring-1 ring-white/5">
             <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
               <div className="border-r border-b border-r-white/10 border-b-white/20 bg-white/5 px-4 py-2 text-white">
