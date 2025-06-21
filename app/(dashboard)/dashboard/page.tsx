f62df7d2b3fd52ef9dbefebef939560b6fb4ca6e
Alexander Cleoni <30603679+ACleoni@users.noreply.github.com>
2025-06-21 13:17:11 -0400

    change theme colors to reflect macrofy branding (#5)

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -241,7 +241,7 @@ function InviteTeamMember() {
           )}
           <Button
             type="submit"
-            className="bg-orange-500 hover:bg-orange-600 text-white"
+            className="bg-emerald-500 hover:bg-emerald-600 text-white"
             disabled={isInvitePending || !isOwner}
           >
             {isInvitePending ? (
